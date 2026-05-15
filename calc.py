5e04b55df3589948b1275c97c86d34741a780e68
nilesh <nilesh112003k@gmail.com>
2026-05-15 15:54:43 +0530

    Initial commit

---
+++ calc.py
@@ -0,0 +1,29 @@
+print("Simple Calculator")
+while True:
+    num1 = float(input("Enter 1st no. : "))
+    op = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter 2nd no. : "))
+
+    if op == '+':
+        print("Result:", num1 + num2)
+
+    elif op == '-':
+        print("Result:", num1 - num2)
+
+    elif op == '*':
+        print("Result:", num1 * num2)
+
+    elif op == '/':
+        if num2 == 0:
+            print("Cannot divide by zero")
+        else:
+            print("Result:", num1 / num2)
+
+    else:
+        print(" operator is not valid ")
+
+    choice = input("\nDo you want to calculate again? (y/n): ")
+    
+    if choice.lower() != 'y':
+        print("Calculator closed.")
+        break 
